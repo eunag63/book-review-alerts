c91fe9f7b00fcb46b327c50b8e21f701bea1d8bb
eunag63 <eunag39@gmail.com>
2025-09-04 15:35:08 +0900

    🚑 사용하지 않는 import 정리

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react'
 import type { Review } from '../lib/types'
 import type { ReviewWithBadge } from '../lib/clickAnalytics'
 import { getReviewsByPeriod, getAvailablePeriods, calcDDay, isCreatedToday, isDeadlineValid } from '../lib/reviewUtils'
-import { getReviewClickCounts, assignBadgesToReviews } from '../lib/clickAnalytics'
+import { assignBadgesToReviews } from '../lib/clickAnalytics'
 // import BannerAd from './components/BannerAd'
 import SearchReviews from './components/SearchReviews'
 
