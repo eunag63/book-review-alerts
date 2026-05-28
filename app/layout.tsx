dea1591d0d834abf00b99eb8e455b7bc9ef5a891
eunag63 <eunag39@gmail.com>
2026-05-28 15:45:10 +0900

    🐛 구글 애널리틱스 추적 코드 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,25 +11,21 @@ export default function RootLayout({
     <html lang="ko">
       <head>
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CX9H8TH5EB"
+        />
+        <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-N5TTMTF6');`,
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CX9H8TH5EB');
+            `,
           }}
         />
       </head>
       <body className="min-h-screen" style={{ backgroundColor: "#0a0a0a" }}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N5TTMTF6"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-
         <Header />
 
         {/* 녹색 구역에 서평단 등록하기 */}
